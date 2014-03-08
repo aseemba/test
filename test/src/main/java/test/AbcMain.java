0351292f93fe9d01565cf3339789b3d3cd9f8e0b
aseemba <belsare.aseem@gmail.com>
2014-03-07 19:23:52 -0600

    first attempt

---
+++ test/src/main/java/test/AbcMain.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author maverick
+ *
+ */
+public class AbcMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("first attempt at creating a project in github");
+	}
+
+}
